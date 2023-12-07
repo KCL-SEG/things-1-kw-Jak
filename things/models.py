7520916b0c0c3187f1996ed281c67536d33bc511
Ka (Ka Wai) Lau <kwlau107@gmail.com>
2023-12-07 00:54:53 +0000

    Modify models.py

--- things/models.py
+++ things/models.py
@@ -5,7 +5,7 @@ from django.db.models import Model
 
 class Thing(Model):
     name = models.CharField(unique=True, blank=False, max_length=30)
-    description = models.TextField(unique=False, blank=True, max_length=120)
+    description = models.CharField(unique=False, blank=True, max_length=120)
     quantity = models.IntegerField(
         unique=False,
         validators=[MinValueValidator(0), MaxValueValidator(100)]
